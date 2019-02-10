7ea95d0f2ff3d7824242229222669e6eb4ac01ee
Roy Lin <44574155+chshroy@users.noreply.github.com>
2019-02-10 21:54:03 +0800

    Create CF_tempelet.cpp

---
+++ CF_tempelet.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+#define rep(i, a, n) for(int i = a; i < n; ++i)
+#define rep0(i, n) rep(i, 0, n)
+#define rep1(i, n) rep(i, 1, n+1)
+#define fs first
+#define sc second
+#define IOS cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define _ << " " << 
+#define wer(x) cout << (#x) << "->" << (x) << endl;
+//////////////////////////////////////////////////////////////////
+
+int main() {
+	
+}
