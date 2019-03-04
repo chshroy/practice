3898e6c402cf3c2c03d57d5a520e9193bdf6c32b
Roy Lin <44574155+chshroy@users.noreply.github.com>
2019-03-04 15:13:41 +0800

    Update disjoint_set.cpp

--- algorithm/disjoint_set.cpp
+++ algorithm/disjoint_set.cpp
@@ -1,22 +1,18 @@
-struct dsu {
-	int n;
-	int p[10005], sz[10005];
-
-	void init() {
-		rep1(i,n) p[i] = i, sz[i] = 1;
-	}
-
-	int find(int x) {
-		return x == p[x] ? x : p[x] = find(p[x]);
-	}
-
-	void uni(int x, int y) {
-		x = find(x), y = find(y);
-		if(sz[x] > sz[y]) swap(x, y);
-		p[x] = y;
-	}
-
-	bool query(int x, int y) {
-		return find(x) == find(y);
-	}
-};
+struct DSU {
+    int p[maxn], sz[maxn];
+    
+    void init() {
+        rep0(i, n) p[i] = i, sz[i] = 1;
+    }    
+    
+    int find(int x) {
+        return x == p[x] ? x : p[x] = find(p[x]);
+    }
+    
+    void uni(int x, int y) {
+        // x, y must be parent
+        if(sz[x] < sz[y]) swap(x, y);
+        p[y] = x;
+        sz[y] += x;
+    }
+} dsu;
