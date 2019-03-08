44b3244f494bc1b74b552ea755f3b654220c034a
Roy Lin <44574155+chshroy@users.noreply.github.com>
2019-03-08 14:31:01 +0800

    Update dinic.cpp

--- algorithm/dinic.cpp
+++ algorithm/dinic.cpp
@@ -1,62 +1,59 @@
-#define INF 2147483647
+const int maxn = 1e6;
 
-const int maxp = 1000005;
+struct edge {
+	int to, rev, cap;
+};
 
-struct edge{ int to,rev,cap; };
-
-struct dinic {
-	
-	int level[maxp],iter[maxp];
-	vector<edge>G[maxp];
-
-	void add_edge(int from, int to, int cap) {
-		G[from].pb({to, (int)G[to].size(), cap});
-		G[to].pb({from, (int)G[from].size()-1, cap});  //注意是無向邊，反邊的cap不為０
+struct DINIC {
+	int s, t;
+	vector<edge> G[maxn];
+	void add_edge(int u, int v, int w) {
+		G[u].pb({v, G[v].size(), w});
+		G[v].pb({u, G[u].size() - 1, w}); // 無向邊 
 	}
-
-	void bfs(int s) {
-		memset(level, -1, sizeof(level));
-		level[s]=0;
-		queue<int>q; q.push(s);
+	int iter[maxn], depth[maxn]; // d -> depth
+	bool bfs() {
+		//cout << "bfs\n";
+		memset(depth, -1, sizeof(depth));
+		queue<int> q; q.push(s);
+		depth[s] = 0;
 		while(!q.empty()) {
-			int v = q.front(); q.pop();
-			rep0(i,(int)G[v].size()) {
-				edge &e=G[v][i];
-				if(e.cap>0 && level[e.to] == -1) {
-					level[e.to] = level[v] + 1;
-					q.push(e.to);
-				}
+			int u = q.front(); q.pop();
+			for(edge e : G[u]) {
+				int v = e.to;
+				if(depth[v] != -1 || e.cap == 0) continue;
+				depth[v] = depth[u] + 1;
+				q.push(v);
 			}
 		}
-		
+		return depth[t] != -1;
 	}
-
 	
-	int dfs(int v, int t, int f) {
-		if(v==t) return f;
-
-		for(int &i=iter[v];i<(int)G[v].size();++i) {
-			edge &e = G[v][i];
-			if(e.cap > 0 && level[v] < level[e.to]) {
-				int d = dfs(e.to,t,min(f,e.cap));
-				if(d > 0) {
-					e.cap -= d;
-					G[e.to][e.rev].cap += d;
-					return d;
-				}
+	int dfs(int pos, int f) {
+		//cout << "dfs\n";
+		if(pos == t) return f;
+		for(int &i = iter[pos]; i < G[pos].size(); ++i) {
+			edge &e = G[pos][i];
+			if(e.cap == 0 || depth[e.to] <= depth[pos]) continue;
+			int d = dfs(e.to, min(f, e.cap));
+			if(d) {
+				e.cap -= d;
+				G[e.to][e.rev].cap += d;
+				return d;
 			}
 		}
 		return 0;
 	}
-
-	int max_flow(int s, int t) {
-		int flow=0;
-		while(1) {
-			bfs(s);
-			if(level[t] == -1) return flow;
+	
+	int max_flow() {
+		int flow = 0;
+		while(bfs()) {
 			memset(iter, 0, sizeof(iter));
-			int f;
-			while((f = dfs(s,t,INF))>0) flow += f;
+			int f = 0;
+			while(f = dfs(s, INT_MAX)) {
+				flow += f;
+			}
 		}
+		return flow;
 	}
-} flow;
+} dinic;
