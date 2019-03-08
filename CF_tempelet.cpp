26a181fb2e3da382dd68a56c56eb8f75269a76ba
Roy Lin <44574155+chshroy@users.noreply.github.com>
2019-03-08 16:54:28 +0800

    Update CF_tempelet.cpp

--- CF_tempelet.cpp
+++ CF_tempelet.cpp
@@ -3,17 +3,20 @@ using namespace std;
 
 typedef long long ll;
 typedef unsigned long long ull;
-typedef pair<int, int> ii;
+typedef pair<ll, ll> ii;
 #define rep(i, a, n) for(int i = a; i < n; ++i)
 #define rep0(i, n) rep(i, 0, n)
 #define rep1(i, n) rep(i, 1, n+1)
 #define fs first
 #define sc second
+#define pb push_back
+#define eb emplace_back
 #define IOS cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
 #define _ << " " << 
 #define wer(x) cout << (#x) << "->" << (x) << endl;
+#define int ll
 //////////////////////////////////////////////////////////////////
 
-int main() {
+signed main() {
 	
 }
