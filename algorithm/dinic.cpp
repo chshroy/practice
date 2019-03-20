f8f3c77785c98c982dc0d7ba42ffdea20bdc0480
Roy Lin <44574155+chshroy@users.noreply.github.com>
2019-03-20 23:12:59 +0800

    Update dinic.cpp

--- algorithm/dinic.cpp
+++ algorithm/dinic.cpp
@@ -1,61 +1,59 @@
-// dinic.s 起點 dinic.t終點
-// dinic.max_flow() 
-const int maxn = 1e6;
-
+// DINIC dinie(s, t)
+// dinic.add_edge()
+// dinic.max_flow
+const int maxn = 10005;
 struct edge {
-	int to, rev, cap;
+    int to, rev, w;
 };
-
 struct DINIC {
-	int s, t;
-	vector<edge> G[maxn];
-	void add_edge(int u, int v, int w) {
-		G[u].pb({v, G[v].size(), w});
-		G[v].pb({u, G[u].size() - 1, w}); // 無向邊 
-	}
-	int iter[maxn], depth[maxn]; // d -> depth
-	bool bfs() {
-		//cout << "bfs\n";
-		memset(depth, -1, sizeof(depth));
-		queue<int> q; q.push(s);
-		depth[s] = 0;
-		while(!q.empty()) {
-			int u = q.front(); q.pop();
-			for(edge e : G[u]) {
-				int v = e.to;
-				if(depth[v] != -1 || e.cap == 0) continue;
-				depth[v] = depth[u] + 1;
-				q.push(v);
-			}
-		}
-		return depth[t] != -1;
-	}
-	
-	int dfs(int pos, int f) {
-		//cout << "dfs\n";
-		if(pos == t) return f;
-		for(int &i = iter[pos]; i < G[pos].size(); ++i) {
-			edge &e = G[pos][i];
-			if(e.cap == 0 || depth[e.to] <= depth[pos]) continue;
-			int d = dfs(e.to, min(f, e.cap));
-			if(d) {
-				e.cap -= d;
-				G[e.to][e.rev].cap += d;
-				return d;
-			}
-		}
-		return 0;
-	}
-	
-	int max_flow() {
-		int flow = 0;
-		while(bfs()) {
-			memset(iter, 0, sizeof(iter));
-			int f = 0;
-			while(f = dfs(s, INT_MAX)) {
-				flow += f;
-			}
-		}
-		return flow;
-	}
-} dinic;
+#define INF INT_MAX
+    int s, t;
+    vector<edge> G[maxn];
+    int lev[maxn], iter[maxn];
+    DINIC(const int &s, const int &t) : s(s), t(t) {}
+    void add_edge(int u, int v, int w) {
+        G[u].pb({v, (int)G[v].size(), w});
+        G[v].pb({u, (int)G[u].size() - 1, 0});
+    }
+
+    bool bfs() {
+        memset(lev, -1, sizeof(lev));
+        lev[s] = 0;
+        queue<int> q; q.push(s);
+        while(!q.empty()) {
+            int u = q.front(); q.pop();
+            for(auto e : G[u]) if(e.w && lev[e.to] == -1) {
+                lev[e.to] = lev[u] + 1;
+                q.push(e.to);
+            }
+        }
+        return lev[t] != -1;
+    }
+
+    int dfs(int u, int f) {
+        if(u == t) return f;
+        for(int &i = iter[u]; i < G[u].size(); ++i) {
+            auto &e = G[u][i];
+            if(e.w && lev[e.to] > lev[u]) {
+                int d = dfs(e.to, min(f, e.w));
+                if(d) {
+                    e.w -= d;
+                    G[e.to][e.rev].w += d;
+                    return d;
+                }
+            }
+        }
+        return 0;
+    }
+
+    int max_flow() {
+        int flow = 0;
+        while(bfs()) {
+            memset(iter, 0, sizeof(iter));
+            int f;
+            while((f = dfs(s, INF))) flow += f;
+        }
+        return flow;
+    }
+    
+};
