e6db49b14cd124b1c7dc8f1813b939f9841a8dac
Roy Lin <44574155+chshroy@users.noreply.github.com>
2019-03-24 15:32:30 +0800

    Update strange.cpp

--- 優化大法/strange.cpp
+++ 優化大法/strange.cpp
@@ -1,3 +1,15 @@
 inline int min(int a, int b) {//最小值
     return a & ((a - b) >> 31) | b & ( ~ (a - b) >> 31);
 }
+
+/*Function to find minimum of x and y*/
+inline int min(int x, int y) 
+{ 
+  return  y + ((x - y) & ((x - y) >> (sizeof(int) * CHAR_BIT - 1)));  
+} 
+  
+/*Function to find maximum of x and y*/
+inline int max(int x, int y) 
+{ 
+  return x - ((x - y) & ((x - y) >> (sizeof(int) * CHAR_BIT - 1))); 
+} 
