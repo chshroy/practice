a0663a660dcb5e91bb1ec3fae84e1c645bb9eadf
Roy Lin <44574155+chshroy@users.noreply.github.com>
2019-03-30 09:08:28 +0800

    Update CF_tempelet.cpp

--- CF_tempelet.cpp
+++ CF_tempelet.cpp
@@ -1,6 +1,6 @@
-/*input 
+/*input
+
 */
-#define rrr
 %:include <bits/stdc++.h>
 %:include <ext/pb_ds/assoc_container.hpp>
 %:include <ext/rope>
@@ -26,8 +26,8 @@ typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_
 %:define wer(...) if(1) <%cerr<<__LINE__<<") "<<__PRETTY_FUNCTION__<<" "<<(#__VA_ARGS__)<<" = ";_do(__VA_ARGS__);%>
 template<typename T> void _do(T &&_x)<%cerr<<_x<<endl;%>
 template<typename T,typename ...S> void _do(T &&_x,S &&..._t)<%cerr<<_x<<", ";_do(_t...);%>
-template<typename _a,typename _b> ostream& operator << (ostream &_s,const pair<_a,_b> &_p)<%return _s<<"("<<_p.X<<","<<_p.Y<<")";%>
-template<typename It> ostream& _OUTC(ostream &_s,It _ita,It _itb) <%_s<<"{"; for(It _it=_ita;_it!=_itb;_it++) _s<<(_it==_ita?"":", ")<<*_it; _s<<"}"; return _s;%>
+template<typename _a,typename _b> ostream& operator << (ostream &_s,const pair<_a,_b> &_p)<%return _s<<'('<<_p.fs<<','<<_p.sc<<')';%>
+template<typename It> ostream& _OUTC(ostream &_s,It _ita,It _itb) <%_s<<"{"; for(It _it=_ita;_it!=_itb;_it++) _s<<(_it==_ita?'':", ")<<*_it; _s<<'}';return _s;%>
 template<typename _a> ostream &operator << (ostream &_s,vector<_a> &_c)<%return _OUTC(_s,ALL(_c));%>
 template<typename _a> ostream &operator << (ostream &_s,set<_a> &_c)<%return _OUTC(_s,ALL(_c));%>
 template<typename _a,typename _b> ostream &operator << (ostream &_s,map<_a,_b> &_c)<%return _OUTC(_s,ALL(_c));%>
@@ -39,7 +39,3 @@ template<typename _t> void pary(_t _a,_t _b)<%_OUTC(cerr,_a,_b);cerr<<endl;%>
 %:define IOS cin.tie(0); ios_base::sync_with_stdio(0);
 %:endif
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-signed main() {
-	IOS
-}
