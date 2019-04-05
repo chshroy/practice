7a99f264b7d778021f16eaa949b9e5b7d7bcfd59
Roy Lin <44574155+chshroy@users.noreply.github.com>
2019-04-05 22:46:55 +0800

    Update CF_tempelet.cpp

--- CF_tempelet.cpp
+++ CF_tempelet.cpp
@@ -12,7 +12,7 @@ typedef long long ll;
 typedef unsigned long long ull;
 typedef pair<ll,ll> ii;
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> Treap;
-%:define rep(i, a, n) for(int i = 0; i < n; ++i)
+%:define rep(i, a, n) for(int i = a; i < n; ++i)
 %:define rep0(i, n) rep(i, 0, n)
 %:define rep1(i, n) rep(i, 1, n + 1)
 %:define SZ(a) (int)a.size()
