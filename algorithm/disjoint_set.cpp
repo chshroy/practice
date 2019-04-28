e0760d33dd84c674048595ecd89628a3a790df7b
Roy Lin <44574155+chshroy@users.noreply.github.com>
2019-04-28 17:30:12 +0800

    Update disjoint_set.cpp

--- algorithm/disjoint_set.cpp
+++ algorithm/disjoint_set.cpp
@@ -9,10 +9,14 @@ struct DSU {
         return x == p[x] ? x : p[x] = find(p[x]);
     }
     
+    bool same(int &x, int &y) {
+        x = find(x), y = find(y);
+        return x == y;
+    }
     void uni(int x, int y) {
         // x, y must be parent
         if(sz[x] < sz[y]) swap(x, y);
         p[y] = x;
         sz[x] += sz[y];
     }
-} dsu;
+};
