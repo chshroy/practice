8a939175bd89afb7fc8230ab6bd2682d500dfeef
Roy Lin <44574155+chshroy@users.noreply.github.com>
2019-08-08 23:45:12 +0200

    Update input_output.cpp

--- template/input_output.cpp
+++ template/input_output.cpp
@@ -37,7 +37,7 @@ namespace output {
 
 	template<typename T, typename... Args> 
 	void ou(const T& t, const Args&... args) { ou(t); ou(' '); ou(args...); }
-};
+}
 using namespace input;
 using namespace output;
 
