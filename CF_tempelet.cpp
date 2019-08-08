40589a399850271b2acaa9125e0e88ac0bb41748
Roy Lin <44574155+chshroy@users.noreply.github.com>
2019-08-08 23:47:24 +0200

    Update CF_tempelet.cpp

--- CF_tempelet.cpp
+++ CF_tempelet.cpp
@@ -1,4 +1,3 @@
-
 #pragma GCC optimize("Ofast")
 
 #include <bits/stdc++.h>
@@ -8,6 +7,7 @@ using namespace std;
 using ii = pair<int, int>;
 using ll = long long;
 using ull = unsigned long long;
+using ld = long double;
 
 #define for_(i, a, n) for(int i = a; i < n; ++i)
 #define forr(i, n) for(int i = n - 1; i >= 0; ++i)
@@ -23,76 +23,77 @@ using ull = unsigned long long;
 #define eb emplace_back
 #define endl '\n'
 #define INF INT_MAX
-#define CASET int ___T; cin >> ___T; for_1(cs, ___T)
+#define CASET int ___T; cin >> ___T; for1(cs, ___T)
 
 namespace input {
-	template<typename T> void re(T&& t) { cin >> t; }
+	template<typename T> bool re(T&& t) { return (cin >> t) ? 1 : 0; }
 	template<typename T1, typename T2>									// pair
-	void re(pair<T1, T2> &&t) { re(t.first); re(t.second); }
+	void re(pair<T1, T2> &t) { re(t.first); re(t.second); }
 
-	template<typename T>
-	void re(int &&n, vector<T> &&v) {									// vector
-		re(n); v.reserve(n); for(int i = 0; i < n; re(v[i++]));
+	template<typename T1, typename T2>									// vector
+	void re_v(const T1 &n, vector<T2> &v) {
+		for(int i = 0; i < n; ++i) {
+			T2 in; re(in);
+			v.push_back(in);
+		}
 	}
 
-	template<typename T, typename... Args> 
+	template<typename T, typename... Args>
 	void re(T&& t, Args&&... args) { re(t); re(args...); }
 }
 
 namespace output {
-	template<typename T> void ou(T&& t) { cout << t; }
+	template<typename T> void ou(const T& t) { cout << t; }
 	template<typename T1, typename T2>									// pair
-	void ou(pair<T1, T2> &&t) { ou(t.first); ou(t.second); }
+	void ou(const pair<T1, T2> &t) { ou(t.first); ou(t.second); }
 
 	template<typename T>
-	void ou(vector<T> &v) {												// vector
-		for(int i = 0; i < (int)v.size(); ou(v[i++])) if(i) ou(' ');
+	void ou_v(const vector<T> &v) {
+		for(auto it = v.begin(); it != v.end(); ou(*it++))				// vector
+			if(it != v.cbegin()) ou(' ');
 	}
 
-	template<typename T, typename... Args> 
-	void ou(T&& t, Args&&... args) { ou(t, ' '); ou(args...); }
-};
+	template<typename T, typename... Args>
+	void ou(const T& t, const Args&... args) { ou(t); ou(' '); ou(args...); }
+}
 using namespace input;
 using namespace output;
-/*
-input(a, b, c);
-input(n, v); // v is vector
-output(a, b, c);
-output(v);
-*/
+
+namespace DEBUG {
+	template<typename T> void _de(const T& t) { cerr << t; }
+	template<typename T1, typename T2>									// pair
+	void _de(const pair<T1, T2> &t) { cerr << '(' << t.first << ", " << t.second << ')'; }
+
+	template<typename T>
+	void pary(T t1, T t2) {
+		cerr << "[";
+		for(auto t = t1; t != t2; _de(*t++))
+			if(t != t1) cerr << ", ";
+		cerr << "]\n";
+	}
+
+	template<typename T, typename... Args>
+	void _de(const T& t, const Args&... args) { cerr << t << ' '; _de(args...); }
+}
 
 #ifdef rrr
-#define wer(...) if(1) {                                                                \
-    fprintf(stderr, "%s - %d (%s) = ", __PRETTY_FUNCTION__, __LINE__, #__VA_ARGS__);    \
-    _do(__VA_ARGS__);                                                                   \
+using namespace DEBUG;
+#define wer(...) if(1) {																\
+	cerr << __PRETTY_FUNCTION__ << " - " << __LINE__ << "(" << #__VA_ARGS__ << ") = ";	\
+	_de(__VA_ARGS__, '\n');																	\
 }
-template<typename T>void _do(T &&_x){ cerr << _x << endl; }
-template<typename T, typename ...S> void _do(T &&_x, S &&..._t){ cerr << _x << " ," ; _do(_t...); }
-template<typename _a,typename _b> ostream& operator << (ostream &_s, const pair<_a, _b> &_p) {
-    return _s<<"("<<_p.X<<","<<_p.Y<<")";
+#define werp(v) if(1) {																	\
+	cerr << __PRETTY_FUNCTION__ << " - " << __LINE__ << "(" << #v << ") = ";			\
+	pary(v.begin(), v.end());															 				\
 }
-template<typename It> ostream& _OUTC(ostream &_s, It _ita, It _itb)
-{
-    _s << '{';
-    for(It _it = _ita; _it != _itb ;_it++)
-    {
-        _s << (_it == _ita ? "" : ",") << *_it;
-    }
-    _s << '}';
-    return _s;
-}
-template<typename _a> ostream &operator << (ostream &_s, vector<_a> &_c){ return _OUTC(_s, ALL(_c)); }
-template<typename _a> ostream &operator << (ostream &_s,set<_a> &_c){ return _OUTC(_s, ALL(_c)); }
-template<typename _a, typename _b> ostream &operator << (ostream &_s, map<_a, _b> &_c){ return _OUTC(_s,ALL(_c)); }
-template<typename _t> void pary(_t _a, _t _b){ _OUTC(cerr, _a, _b); cerr << endl; }
 #define IOS
 #else
 #define wer(...)
+#define werp(...)
 #define pary(...)
-#define IOS cin.tie(0); ios_base::sync_with_stdio(0);
+#define IOS cin.tie(NULL); ios_base::sync_with_stdio(NULL);
 #endif
-// roy ya hahahahahahahahahahahahahaha
 
-signed main() { IOS
+signed main { IOS
 
 }
